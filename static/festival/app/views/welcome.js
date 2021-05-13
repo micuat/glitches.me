cea3b1ad27c811af7421296c583f6bac1c76d752
micuat <micuat@gmail.com>
2021-05-13 19:37:18 +0000

    refactor

--- static/festival/app/views/welcome.js
+++ static/festival/app/views/welcome.js
@@ -1,11 +1,126 @@
 // import choo's template helper
 const html = require("choo/html");
 
+const schedule = [
+  {
+    title: "April 25, 2021: Soft Opening (Website Launch)",
+    url: "",
+    img: "https://img.glitches.me/images/2021/04/26/image7d400fea9c7b7cd5.png"
+  },
+  {
+    title: "April 26, 2021 - March 31, 2022: Collective depressing session (24/7)",
+    url: "https://depression.glitches.me/",
+    img: "https://img.glitches.me/images/2021/04/26/image8daef8001ccc12ac.png"
+  },
+  {
+    title: `April 27, 2021: Presentation at "Ctrl-Space"`,
+    url: "",
+    img: "https://img.glitches.me/images/2021/04/27/Screenshot-from-2021-04-27-13-05-21.png"
+  },
+  {
+    title: `April 28, 2021: Best Practices in Contemporary Dance: Chat (Jorge Guevara & Naoto Hieda)`,
+    url: "https://www.youtube.com/watch?v=kC-7hzK0jgM",
+    img: "https://img.glitches.me/images/2021/04/28/vlcsnap-2021-04-28-17h25m17s756.png"
+  },
+  {
+    title: `April 28, 2021: Intervention in Arium`,
+    url: "https://arium.xyz/spaces/nh",
+    img: "https://assets.vlts.pw/spaceUserAssets/nh/LQRdaF5y37cNtpe646xWF4GUXit2/default-meta-mlQBma4e.jpg"
+  },
+  {
+    title: `April 30, 2021: hydra-xml`,
+    url: "https://hydra-xml.glitch.me/",
+    img: "https://img.glitches.me/images/2021/04/30/Screenshot-from-2021-04-30-22-53-14.png"
+  },
+  {
+    title: `May 1, 2021: Best Practices in Contemporary Dance: Development, Practice & Chat (Jorge Guevara & Naoto Hieda)`,
+    url: "https://youtu.be/fqxNOgyhucI",
+    img: "https://img.glitches.me/images/2021/05/01/vlcsnap-2021-05-01-17h41m49s064.png"
+  },
+  {
+    title: `May 3, 2021: Best Practices in Contemporary Dance: Chat (Jorge Guevara & Naoto Hieda)`,
+    url: "https://youtu.be/Kx8lVCxXLgA",
+    img: "https://img.glitches.me/images/2021/05/03/vlcsnap-2021-05-03-17h36m46s282.png"
+  },
+  {
+    title: `May 4, 2021: Presentation at "Cats"`,
+    url: "",
+    img: "https://img.glitches.me/images/2021/05/04/vlcsnap-2021-05-04-21h56m37s298.png"
+  },
+  {
+    title: `May 5, 2021: Presentation at "Together"`,
+    url: "",
+    img: "https://img.glitches.me/images/2021/05/13/vlcsnap-2021-05-13-20h55m42s813.png"
+  },
+  {
+    title: `May 6, 2021: Hydra A-Frame`,
+    url: "https://hydra-aframe-anime-4.glitch.me/",
+    img: "https://img.glitches.me/images/2021/05/07/imagef5eb42879c4ef467.png"
+  },
+  {
+    title: `May 7, 2021: Presentation at "Gifts"`,
+    url: "",
+    img: "https://img.glitches.me/images/2021/05/07/image.png"
+  },
+  {
+    title: `May 8, 2021: Best Practices in Contemporary Dance: Chat (Jorge Guevara & Naoto Hieda)`,
+    url: "https://youtu.be/ENmQK0ZCbQw",
+    img: "https://img.glitches.me/images/2021/05/08/vlcsnap-2021-05-08-19h38m34s258.png"
+  },
+  {
+    title: `May 8, 2021: Best Practices... But Alone`,
+    url: "https://youtu.be/oGHAVVSmAwc",
+    img: "https://img.glitches.me/images/2021/05/09/image.png"
+  },
+  {
+    title: `May 10, 2021: Best Practices in Contemporary Dance: Chat (Jorge Guevara & Naoto Hieda)`,
+    url: "https://youtu.be/wafVx8RiDeE",
+    img: "https://img.glitches.me/images/2021/05/11/vlcsnap-2021-05-11-01h26m22s384.png"
+  },
+  {
+    title: `May 10, 2021: Flok Replay (Flor de Fuego & Naoto Hieda)`,
+    url: "https://flok-replay.glitch.me/",
+    img: "https://img.glitches.me/images/2021/05/11/image.png"
+  },
+  {
+    title: `May 11, 2021: Razio (Guest: Luise Flügge)`,
+    url: "https://razio.glitch.me/luise",
+    img: "https://cdn.glitch.com/fce09100-6702-45dd-b818-65dc2117c886%2Fvlcsnap-2021-05-11-23h14m32s972.png?v=1620767699624"
+  },
+  {
+    title: `May 12, 2021: Flok Replay (Flor de Fuego & Naoto Hieda)`,
+    url: "https://flok-replay.glitch.me/",
+    img: "https://img.glitches.me/images/2021/05/13/image.png"
+  },
+  {
+    title: `May 13, 2021: Best Practices in Contemporary Dance: Development & Practice (Jorge Guevara & Naoto Hieda)`,
+    url: "https://youtu.be/fqxNOgyhucI",
+    img: "https://img.glitches.me/images/2021/05/13/vlcsnap-2021-05-13-21h04m32s366.png"
+  },
+  {
+    title: `May 13, 2021: Best Practices in Contemporary Dance: Chat (Joana Chicau, Jorge Guevara & Naoto Hieda)`,
+    url: "https://youtu.be/fqxNOgyhucI",
+    img: "https://img.glitches.me/images/2021/05/13/vlcsnap-2021-05-13-21h02m48s927.png"
+  },
+]
+
 // export module
 module.exports = function (state, emit) {
   state.bighead = p([`festival.`, `glitches.me`])
     .color([0, 0.5], 0.1, 1).shadow().bg([1, 0], 1, 0).size(40)//.parent("#bighead")
     .font("VT323").scrollX([0, 0.1]).out(0)
+
+  const scheduleDom = schedule.map((e) => e.url.length > 0 ? html`
+  <li><a href="${e.url}">
+  <img src="${e.img}">
+  ${e.title}
+  </a></li>` : html`
+  <li>
+  <img src="${e.img}">
+  ${e.title}
+  </li>`
+  )
+
   const blah = html`The first edition of <b>festival.glitches.me</b> is an independent festival that investigates body, network and digitality through manifestation and question around institutions and the current society that form what is perceived as artistic activities and critically propose methodologies to speculate ideas that pose significant impact to cultural domains.`
   return html`
     <div>
@@ -42,52 +157,7 @@ module.exports = function (state, emit) {
     <h2>Events</h2>
     <div class="gridy">
       <ul>
-        <li>
-        <img src="https://img.glitches.me/images/2021/04/26/image7d400fea9c7b7cd5.png">
-        April 25, 2021: Soft Opening (Website Launch)</li>
-        <li><a href="https://depression.glitches.me/">
-        <img src="https://img.glitches.me/images/2021/04/26/image8daef8001ccc12ac.png">
-        April 26, 2021 - March 31, 2022: Collective depressing session (24/7)
-        </a></li>
-        <li>
-        <img src="https://img.glitches.me/images/2021/04/27/Screenshot-from-2021-04-27-13-05-21.png">
-        April 27, 2021: Presentation at "Ctrl-Space"</li>
-        <li><a href="https://www.youtube.com/watch?v=kC-7hzK0jgM">
-        <img src="https://img.glitches.me/images/2021/04/28/vlcsnap-2021-04-28-17h25m17s756.png">
-        April 28, 2021: Best Practices in Contemporary Dance (Chat)</a></li>
-        <li><a href="https://arium.xyz/spaces/nh">
-        <img src="https://assets.vlts.pw/spaceUserAssets/nh/LQRdaF5y37cNtpe646xWF4GUXit2/default-meta-mlQBma4e.jpg">
-        April 28, 2021: Intervention in Arium</a></li>
-        <li><a href="https://hydra-xml.glitch.me/">
-        <img src="https://img.glitches.me/images/2021/04/30/Screenshot-from-2021-04-30-22-53-14.png">
-        April 30, 2021: hydra-xml</a></li>
-        <li><a href="https://youtu.be/fqxNOgyhucI">
-        <img src="https://img.glitches.me/images/2021/05/01/vlcsnap-2021-05-01-17h41m49s064.png">
-        May 1, 2021: Best Practices in Contemporary Dance (Development, Practice & Chat)</a></li>
-        <li><a href="https://youtu.be/Kx8lVCxXLgA">
-        <img src="https://img.glitches.me/images/2021/05/03/vlcsnap-2021-05-03-17h36m46s282.png">
-        May 3, 2021: Best Practices in Contemporary Dance (Chat)</a></li>
-        <li>
-        <img src="https://img.glitches.me/images/2021/05/04/vlcsnap-2021-05-04-21h56m37s298.png">
-        May 4, 2021: Presentation at "Cats"</li>
-        <li>
-        <img src="https://img.glitches.me/images/2021/05/05/image.png">
-        May 5, 2021: Presentation at "Together"</li>
-        <li><a href="https://hydra-aframe-anime-4.glitch.me/">
-        <img src="https://img.glitches.me/images/2021/05/07/imagef5eb42879c4ef467.png">
-        May 6, 2021: Hydra A-Frame</a></li>
-        <li>
-        <img src="https://img.glitches.me/images/2021/05/07/image.png">
-        May 7, 2021: Presentation at "Gifts"</li>
-        <li><a href="https://youtu.be/ENmQK0ZCbQw">
-        <img src="https://img.glitches.me/images/2021/05/08/vlcsnap-2021-05-08-19h38m34s258.png">
-        May 8, 2021: Best Practices in Contemporary Dance (Chat)</a></li>
-        <li><a href="https://youtu.be/oGHAVVSmAwc">
-        <img src="https://img.glitches.me/images/2021/05/09/image.png">
-        May 8, 2021: Best Practices... But Alone</a></li>
-        <li><a href="https://youtu.be/wafVx8RiDeE">
-        <img src="https://img.glitches.me/images/2021/05/11/vlcsnap-2021-05-11-01h26m22s384.png">
-        May 10, 2021: Best Practices in Contemporary Dance (Chat)</a></li>
+        ${scheduleDom}
       </ul>
     </div>
     </section>

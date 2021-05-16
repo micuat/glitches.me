468cf8689e610bc87826872a258a374af5ae3fad
micuat <micuat@gmail.com>
2021-05-16 10:30:44 +0000

    format

---
+++ static/festival/app/views/schedule.js
@@ -0,0 +1,122 @@
+module.exports = [
+  {
+    title: `May 15, 2021: Intervention at CityLeaks (Naoto Hieda intervening/hijacking installation by Cylvester)`,
+    url: "https://www.instagram.com/p/CO5uQLGqKQd/",
+    img: "https://img.glitches.me/images/2021/05/15/image.png"
+  },
+  {
+    title: `May 15, 2021: Razio (Guest: So Kanno)`,
+    url: "https://razio.glitch.me/so",
+    img: "https://cdn.glitch.com/fce09100-6702-45dd-b818-65dc2117c886%2Fvlcsnap-2021-05-15-18h07m38s102.png?v=1621094936408"
+  },
+  {
+    title: `May 15, 2021: Best Practices in Contemporary Dance: Chat (Jorge Guevara & Naoto Hieda)`,
+    url: "https://youtu.be/Sxw5jV1SzFI",
+    img: "https://img.glitches.me/images/2021/05/15/vlcsnap-2021-05-15-21h56m39s551.png"
+  },
+  {
+    title: `May 14, 2021: Best Practices... But Alone (Naoto Hieda)`,
+    url: "https://youtu.be/V_GIrQpE1Xs",
+    img: "https://img.glitches.me/images/2021/05/14/image70adc94baabfc226.png"
+  },
+  {
+    title: `May 13, 2021: Best Practices in Contemporary Dance: Chat (Joana Chicau, Jorge Guevara & Naoto Hieda)`,
+    url: "https://youtu.be/fqxNOgyhucI",
+    img: "https://img.glitches.me/images/2021/05/13/vlcsnap-2021-05-13-21h02m48s927.png"
+  },
+  {
+    title: `May 13, 2021: Best Practices in Contemporary Dance: Development & Practice (Jorge Guevara & Naoto Hieda)`,
+    url: "https://youtu.be/fqxNOgyhucI",
+    img: "https://img.glitches.me/images/2021/05/13/vlcsnap-2021-05-13-21h04m32s366.png"
+  },
+  {
+    title: `May 12, 2021: Flok Replay (Flor de Fuego & Naoto Hieda)`,
+    url: "https://flok-replay.glitch.me/",
+    img: "https://img.glitches.me/images/2021/05/13/image.png"
+  },
+  {
+    title: `May 11, 2021: Razio (Guest: Luise Flügge)`,
+    url: "https://razio.glitch.me/luise",
+    img: "https://cdn.glitch.com/fce09100-6702-45dd-b818-65dc2117c886%2Fvlcsnap-2021-05-11-23h14m32s972.png?v=1620767699624"
+  },
+  {
+    title: `May 10, 2021: Flok Replay (Flor de Fuego & Naoto Hieda)`,
+    url: "https://flok-replay.glitch.me/",
+    img: "https://img.glitches.me/images/2021/05/11/image.png"
+  },
+  {
+    title: `May 10, 2021: Best Practices in Contemporary Dance: Chat (Jorge Guevara & Naoto Hieda)`,
+    url: "https://youtu.be/wafVx8RiDeE",
+    img: "https://img.glitches.me/images/2021/05/11/vlcsnap-2021-05-11-01h26m22s384.png"
+  },
+  {
+    title: `May 8, 2021: Best Practices... But Alone (Naoto Hieda)`,
+    url: "https://youtu.be/oGHAVVSmAwc",
+    img: "https://img.glitches.me/images/2021/05/09/image.png"
+  },
+  {
+    title: `May 8, 2021: Best Practices in Contemporary Dance: Chat (Jorge Guevara & Naoto Hieda)`,
+    url: "https://youtu.be/ENmQK0ZCbQw",
+    img: "https://img.glitches.me/images/2021/05/08/vlcsnap-2021-05-08-19h38m34s258.png"
+  },
+  {
+    title: `May 7, 2021: Presentation at "Gifts" (Naoto Hieda)`,
+    url: "",
+    img: "https://img.glitches.me/images/2021/05/07/image.png"
+  },
+  {
+    title: `May 6, 2021: Hydra A-Frame (Naoto Hieda)`,
+    url: "https://hydra-aframe-anime-4.glitch.me/",
+    img: "https://img.glitches.me/images/2021/05/07/imagef5eb42879c4ef467.png"
+  },
+  {
+    title: `May 5, 2021: Presentation at "Together" (Naoto Hieda)`,
+    url: "",
+    img: "https://img.glitches.me/images/2021/05/13/vlcsnap-2021-05-13-20h55m42s813.png"
+  },
+  {
+    title: `May 4, 2021: Presentation at "Cats" (Naoto Hieda)`,
+    url: "",
+    img: "https://img.glitches.me/images/2021/05/04/vlcsnap-2021-05-04-21h56m37s298.png"
+  },
+  {
+    title: `May 3, 2021: Best Practices in Contemporary Dance: Chat (Jorge Guevara & Naoto Hieda)`,
+    url: "https://youtu.be/Kx8lVCxXLgA",
+    img: "https://img.glitches.me/images/2021/05/03/vlcsnap-2021-05-03-17h36m46s282.png"
+  },
+  {
+    title: `May 1, 2021: Best Practices in Contemporary Dance: Development, Practice & Chat (Jorge Guevara & Naoto Hieda)`,
+    url: "https://youtu.be/fqxNOgyhucI",
+    img: "https://img.glitches.me/images/2021/05/01/vlcsnap-2021-05-01-17h41m49s064.png"
+  },
+  {
+    title: `April 30, 2021: hydra-xml (Naoto Hieda)`,
+    url: "https://hydra-xml.glitch.me/",
+    img: "https://img.glitches.me/images/2021/04/30/Screenshot-from-2021-04-30-22-53-14.png"
+  },
+  {
+    title: `April 28, 2021: Intervention in Arium (Naoto Hieda)`,
+    url: "https://arium.xyz/spaces/nh",
+    img: "https://assets.vlts.pw/spaceUserAssets/nh/LQRdaF5y37cNtpe646xWF4GUXit2/default-meta-mlQBma4e.jpg"
+  },
+  {
+    title: `April 28, 2021: Best Practices in Contemporary Dance: Chat (Jorge Guevara & Naoto Hieda)`,
+    url: "https://www.youtube.com/watch?v=kC-7hzK0jgM",
+    img: "https://img.glitches.me/images/2021/04/28/vlcsnap-2021-04-28-17h25m17s756.png"
+  },
+  {
+    title: `April 27, 2021: Presentation at "Ctrl-Space (Naoto Hieda)"`,
+    url: "",
+    img: "https://img.glitches.me/images/2021/04/27/Screenshot-from-2021-04-27-13-05-21.png"
+  },
+  {
+    title: "April 26, 2021: Collective depressing session (24/7)",
+    url: "https://depression.glitches.me/",
+    img: "https://img.glitches.me/images/2021/04/26/image8daef8001ccc12ac.png"
+  },
+  {
+    title: "April 25, 2021: Soft Opening (Website Launch)",
+    url: "",
+    img: "https://img.glitches.me/images/2021/04/26/image7d400fea9c7b7cd5.png"
+  },
+]
